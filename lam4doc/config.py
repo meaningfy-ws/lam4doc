ab582f5d780fcf27fe827d27dc31137fc34fd30a
Mihai <coslet.mihai@gmail.com>
2020-12-17 19:58:42 +0200

    Add get report ui view
    - add api_wrapper.py method

--- lam4doc/config.py
+++ lam4doc/config.py
@@ -15,13 +15,20 @@ from pathlib import Path
 LAM_DEBUG = os.environ.get('LAM_DEBUG', True)
 LAM_FLASK_SECRET_KEY = os.environ.get('LAM_FLASK_SECRET_KEY', 'secret key')
 
+LAM_API_LOCATION = os.environ.get('LAM_API_LOCATION', 'http://lam-api')
+LAM_API_PORT = os.environ.get('LAM_API_PORT', 4050)
+LAM_API_SERVICE = f'{LAM_API_LOCATION}:{LAM_API_PORT}'
+
 LAM_FUSEKI_LOCATION = os.environ.get('LAM_FUSEKI_LOCATION', 'http://fuseki')
-LAM_FUSEKI_PORT = os.environ.get('LAM_FUSEKI_PORT', '3030')
+LAM_FUSEKI_PORT = os.environ.get('LAM_FUSEKI_PORT', 3030)
 LAM_FUSEKI_QUERY_URL = os.environ.get('LAM_FUSEKI_QUERY_URL', '/lam/query')
 # LAM_FUSEKI_SERVICE = 'http://dev.meaningfy.ws:3030/lam/query'
 LAM_FUSEKI_SERVICE = f'{LAM_FUSEKI_LOCATION}:{LAM_FUSEKI_PORT}{LAM_FUSEKI_QUERY_URL}'
 
 LAM_REPORT_TEMPLATE_LOCATION = str(Path(__file__).parents[1] / 'templates/content')
+
+LAM_DEFAULT_TIMEOUT = int(os.environ.get('LAM_GUNICORN_TIMEOUT', 300))
+
 
 LAM_LOGGER = 'lam'
 
--- lam4doc/entrypoints/ui/views.py
+++ lam4doc/entrypoints/ui/views.py
@@ -8,11 +8,39 @@
 """
 UI pages
 """
-from flask import render_template
+import logging
+import tempfile
+from pathlib import Path
 
+from flask import render_template, send_from_directory, flash
+
+from lam4doc.config import LAM_LOGGER
 from lam4doc.entrypoints.ui import app
+from lam4doc.entrypoints.ui.api_wrapper import get_lam_report as api_get_lam_report
+
+logger = logging.getLogger(LAM_LOGGER)
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', title='LAM index page')
+
+
+@app.route('/lam-report')
+def download_lam_report():
+    logger.debug('request LAM report view')
+    try:
+        with tempfile.TemporaryDirectory() as temp_folder:
+            file_name = f'LAM-report.html'
+            report_content, _ = api_get_lam_report()
+            report = Path(temp_folder) / file_name
+            report.write_bytes(report_content)
+            logger.debug('render LAM report view')
+            return send_from_directory(Path(temp_folder), file_name, as_attachment=True)
+    except Exception as e:
+        logger.exception(str(e))
+
+        flash(str(e), 'error')
+
+        logger.debug('redirect to index view')
+        return render_template('index.html')
