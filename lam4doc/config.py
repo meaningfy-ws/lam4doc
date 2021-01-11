dc9c5777353dae7bb6889a93f55ddd968e83a603
Mihai <coslet.mihai@gmail.com>
2021-01-11 19:14:26 +0200

    Rename cls to self in LAMValidator class methods

--- lam4doc/config.py
+++ lam4doc/config.py
@@ -23,122 +23,122 @@ class LAMConfig:
     logger = logging.getLogger(logger_name)
 
     @property
-    def LAM_LOGGER(cls) -> str:
-        value = cls.logger_name
-        cls.logger.debug(value)
+    def LAM_LOGGER(self) -> str:
+        value = self.logger_name
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_DEBUG(cls) -> str:
+    def LAM_DEBUG(self) -> str:
         value = os.environ.get('LAM_DEBUG', True)
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_FLASK_SECRET_KEY(cls) -> str:
+    def LAM_FLASK_SECRET_KEY(self) -> str:
         value = os.environ.get('LAM_FLASK_SECRET_KEY', 'secret key')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_API_SERVICE(cls) -> str:
+    def LAM_API_SERVICE(self) -> str:
         location = os.environ.get('LAM_API_LOCATION', 'http://lam-api')
         port = os.environ.get('LAM_API_PORT', 4050)
         value = f'{location}:{port}'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_FUSEKI_SERVICE(cls) -> str:
+    def LAM_FUSEKI_SERVICE(self) -> str:
         location = os.environ.get('LAM_FUSEKI_LOCATION', 'http://fuseki')
         port = os.environ.get('LAM_FUSEKI_PORT', 3030)
         value = f'{location}:{port}'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_FUSEKI_REPORT_URL(cls) -> str:
+    def LAM_FUSEKI_REPORT_URL(self) -> str:
         location = os.environ.get('LAM_FUSEKI_LOCATION', 'http://fuseki')
         port = os.environ.get('LAM_FUSEKI_PORT', 3030)
         query_url = os.environ.get('LAM_FUSEKI_QUERY_URL', '/lam/query')
         value = f'{location}:{port}{query_url}'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_FUSEKI_USERNAME(cls) -> str:
+    def LAM_FUSEKI_USERNAME(self) -> str:
         value = os.environ.get('LAM_FUSEKI_USERNAME', 'admin')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_FUSEKI_PASSWORD(cls) -> str:
+    def LAM_FUSEKI_PASSWORD(self) -> str:
         value = os.environ.get('LAM_FUSEKI_PASSWORD', 'admin')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_HTML_REPORT_TEMPLATE_LOCATION(cls) -> str:
+    def LAM_HTML_REPORT_TEMPLATE_LOCATION(self) -> str:
         value = str(Path(__file__).parents[1] / 'templates/html')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_PDF_REPORT_TEMPLATE_LOCATION(cls) -> str:
+    def LAM_PDF_REPORT_TEMPLATE_LOCATION(self) -> str:
         # correct value will be used when pdf implementation is ready
         value = str(Path(__file__).parents[1] / 'templates/html')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_INDEXES_TEMPLATE_LOCATION(cls) -> str:
+    def LAM_INDEXES_TEMPLATE_LOCATION(self) -> str:
         value = str(Path(__file__).parents[1] / 'templates/indexes')
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_CELEX_CONFIG_NAME(cls) -> str:
+    def LAM_CELEX_CONFIG_NAME(self) -> str:
         value = 'celex.json'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_CLASSES_CONFIG_NAME(cls) -> str:
+    def LAM_CLASSES_CONFIG_NAME(self) -> str:
         value = 'classes.json'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_PROPERTIES_CONFIG_NAME(cls) -> str:
+    def LAM_PROPERTIES_CONFIG_NAME(self) -> str:
         value = 'properties.json'
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_DEFAULT_TIMEOUT(cls) -> str:
+    def LAM_DEFAULT_TIMEOUT(self) -> str:
         value = int(os.environ.get('LAM_GUNICORN_TIMEOUT', 300))
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_DOCUMENT_PROPERTY_GRAPH(cls) -> str:
+    def LAM_DOCUMENT_PROPERTY_GRAPH(self) -> str:
         value = os.environ.get("LAM_DOCUMENT_PROPERTY_GRAPH",
                                "http://publications.europa.eu/resources/authority/lam/DocumentProperty")
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_CLASSES_GRAPH(cls) -> str:
+    def LAM_CLASSES_GRAPH(self) -> str:
         value = os.environ.get("LAM_CLASSES_GRAPH",
                                "http://publications.europa.eu/resources/authority/lam/LAMLegalDocument")
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
     @property
-    def LAM_CELEX_CLASSES_GRAPH(cls) -> str:
+    def LAM_CELEX_CLASSES_GRAPH(self) -> str:
         value = os.environ.get("LAM_CELEX_CLASSES_GRAPH",
                                "http://publications.europa.eu/resources/authority/celex/CelexLegalDocument")
-        cls.logger.debug(value)
+        self.logger.debug(value)
         return value
 
 
