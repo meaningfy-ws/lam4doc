b0b42a0c08fef2d0bcf9a5d6ee3cecba09e69684
Laurentiu Mandru <mclaurentiu79@gmail.com>
2021-01-17 16:14:49 +0200

    Uncommented code

--- tests/unit/test_api_handlers.py
+++ tests/unit/test_api_handlers.py
@@ -1,27 +1,27 @@
-# #!/usr/bin/python3
-#
-# # sparql_adapter.py
-# # Date:  27/12/2020
-# # Author: Laurentiu Mandru
-# # Email: mclaurentiu79@gmail.com
-# import pytest
-# from werkzeug.exceptions import BadRequest, UnprocessableEntity
-#
-# from lam4doc.entrypoints.api.handlers import upload_rdfs, generate_lam_report
-#
-#
-# def test_upload_rdfs_at_least_one_file_mandatory():
-#     with pytest.raises(BadRequest) as http_error:
-#         _ = upload_rdfs({"dataset_name": "test"}, None, None, None)
-#
-#     assert http_error.value.code == 400
-#     assert 'Please supply at least one file: lam_properties_document, lam_classes_document, celex_classes_document' \
-#            in http_error.value.description
-#
-#
-# def test_generate_lam_report_exception_not_supported():
-#     with pytest.raises(UnprocessableEntity) as http_error:
-#         _ = generate_lam_report('jpeg')
-#
-#     assert http_error.value.code == 422
-#     assert 'Wrong report_extension format. Accepted formats: html, pdf' in http_error.value.description
+#!/usr/bin/python3
+
+# sparql_adapter.py
+# Date:  27/12/2020
+# Author: Laurentiu Mandru
+# Email: mclaurentiu79@gmail.com
+import pytest
+from werkzeug.exceptions import BadRequest, UnprocessableEntity
+
+from lam4doc.entrypoints.api.handlers import upload_rdfs, generate_lam_report
+
+
+def test_upload_rdfs_at_least_one_file_mandatory():
+    with pytest.raises(BadRequest) as http_error:
+        _ = upload_rdfs({"dataset_name": "test"}, None, None, None)
+
+    assert http_error.value.code == 400
+    assert 'Please supply at least one file: lam_properties_document, lam_classes_document, celex_classes_document' \
+           in http_error.value.description
+
+
+def test_generate_lam_report_exception_not_supported():
+    with pytest.raises(UnprocessableEntity) as http_error:
+        _ = generate_lam_report('jpeg')
+
+    assert http_error.value.code == 422
+    assert 'Wrong report_extension format. Accepted formats: html, pdf' in http_error.value.description
--- tests/unit/test_service_handlers.py
+++ tests/unit/test_service_handlers.py
@@ -1,41 +1,41 @@
-# #!/usr/bin/python3
-#
-# # test_service_handlers.py
-# # Date:  18/12/2020
-# # Author: Mihai Coșleț
-# # Email: coslet.mihai@gmail.com
-# from pathlib import Path
-#
-# import pytest
-#
-# from lam4doc.config import HTML_REPORT_TYPE, PDF_REPORT_TYPE
-# from lam4doc.services.handlers import generate_report, get_report_location, ReportTypeError
-# from tests.conftest import FakeReportBuilder
-#
-#
-# def test_generate_report(tmpdir):
-#     temp_folder = tmpdir.mkdir('report')
-#     report_builder = FakeReportBuilder(temp_folder, 'main.html')
-#
-#     report_path = generate_report(temp_folder, report_builder)
-#
-#     assert Path(temp_folder) / 'main.html' == report_path
-#     assert report_builder.actions[0] == ('MAKE DOCUMENT', str(temp_folder))
-#
-#
-# def test_get_report_location_html():
-#     report_location = get_report_location(HTML_REPORT_TYPE)
-#     expected_location = 'templates/html'
-#     assert report_location[-len(expected_location):] == expected_location
-#
-#
-# def test_get_report_location_pdf():
-#     report_location = get_report_location(PDF_REPORT_TYPE)
-#     expected_location = 'templates/html'
-#     assert report_location[-len(expected_location):] == expected_location
-#
-#
-# def test_get_report_location_failure():
-#     with pytest.raises(ReportTypeError) as e:
-#         _ = get_report_location('mp3')
-#     assert 'No acceptable report template location found' in str(e)
+#!/usr/bin/python3
+
+# test_service_handlers.py
+# Date:  18/12/2020
+# Author: Mihai Coșleț
+# Email: coslet.mihai@gmail.com
+from pathlib import Path
+
+import pytest
+
+from lam4doc.config import HTML_REPORT_TYPE, PDF_REPORT_TYPE
+from lam4doc.services.handlers import generate_report, get_report_location, ReportTypeError
+from tests.conftest import FakeReportBuilder
+
+
+def test_generate_report(tmpdir):
+    temp_folder = tmpdir.mkdir('report')
+    report_builder = FakeReportBuilder(temp_folder, 'main.html')
+
+    report_path = generate_report(temp_folder, report_builder)
+
+    assert Path(temp_folder) / 'main.html' == report_path
+    assert report_builder.actions[0] == ('MAKE DOCUMENT', str(temp_folder))
+
+
+def test_get_report_location_html():
+    report_location = get_report_location(HTML_REPORT_TYPE)
+    expected_location = 'templates/html'
+    assert report_location[-len(expected_location):] == expected_location
+
+
+def test_get_report_location_pdf():
+    report_location = get_report_location(PDF_REPORT_TYPE)
+    expected_location = 'templates/html'
+    assert report_location[-len(expected_location):] == expected_location
+
+
+def test_get_report_location_failure():
+    with pytest.raises(ReportTypeError) as e:
+        _ = get_report_location('mp3')
+    assert 'No acceptable report template location found' in str(e)
